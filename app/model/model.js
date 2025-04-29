c6e21d1c00f1fff60a436d5d9d2b301fd9a12075
IzaakGough <izaakgough@gmail.com>
2025-04-29 09:55:32 +0100

    changes comment_count to INT

--- app/model/model.js
+++ app/model/model.js
@@ -40,7 +40,7 @@ exports.selectArticles = () => {
         articles.created_at,
         articles.votes,
         articles.article_img_url,
-        COUNT(comments.comment_id) AS comment_count
+        COUNT(comments.comment_id)::INT AS comment_count
         FROM articles
         LEFT JOIN 
         comments ON articles.article_id = comments.article_id
